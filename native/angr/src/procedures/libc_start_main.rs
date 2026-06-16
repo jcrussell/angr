d6a8e452a43bceb97361913e5ade2bfe2a8b6c95
Jon Crussell <joncrussell@gmail.com>
2026-06-16 02:26:33 +0000

    refactor: extract procedures/libc_start_main.rs mod tests into libc_start_main_tests.rs sibling

--- native/angr/src/procedures/libc_start_main.rs
+++ native/angr/src/procedures/libc_start_main.rs
@@ -49,29 +49,5 @@ impl NativeSimProcedure for NativeLibcStartMain {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_libc_start_main_no_return() {
-        let proc = NativeLibcStartMain;
-        assert_eq!(proc.name(), "__libc_start_main");
-        assert_eq!(proc.num_args(), 5);
-        assert!(proc.no_return());
-    }
-
-    #[test]
-    fn test_libc_start_main_returns_none() {
-        let mut state = RustSimState::new("amd64").unwrap();
-        let proc = NativeLibcStartMain;
-        let args = vec![
-            RustBV::concrete(0x400000, 64),   // main
-            RustBV::concrete(1, 64),          // argc
-            RustBV::concrete(0x7fff0000, 64), // argv
-            RustBV::concrete(0, 64),          // init
-            RustBV::concrete(0, 64),          // fini
-        ];
-        let result = proc.call(&mut state, &args).unwrap();
-        assert!(result.is_none());
-    }
-}
+#[path = "libc_start_main_tests.rs"]
+mod tests;
