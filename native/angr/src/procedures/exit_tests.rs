4f5807e7dd20de8a6f3416d1de0434851eb6d392
Jon Crussell <joncrussell@gmail.com>
2026-06-16 01:42:59 +0000

    refactor: extract procedures/exit.rs mod tests into exit_tests.rs sibling

---
+++ native/angr/src/procedures/exit_tests.rs
@@ -0,0 +1,50 @@
+use super::*;
+
+#[test]
+fn test_exit_no_return() {
+    let proc = NativeExit;
+    assert!(proc.no_return());
+    assert_eq!(proc.num_args(), 1);
+    assert_eq!(proc.name(), "exit");
+}
+
+#[test]
+fn test_exit_returns_none() {
+    let mut state = RustSimState::new("amd64").unwrap();
+    let result = NativeExit
+        .call(&mut state, &[RustBV::concrete(0, 32)])
+        .unwrap();
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_underscore_exit() {
+    let proc = NativeUnderscoreExit;
+    assert!(proc.no_return());
+    assert_eq!(proc.name(), "_exit");
+    let mut state = RustSimState::new("amd64").unwrap();
+    let result = proc.call(&mut state, &[RustBV::concrete(1, 32)]).unwrap();
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_abort() {
+    let proc = NativeAbort;
+    assert!(proc.no_return());
+    assert_eq!(proc.num_args(), 0);
+    assert_eq!(proc.name(), "abort");
+    let mut state = RustSimState::new("amd64").unwrap();
+    let result = proc.call(&mut state, &[]).unwrap();
+    assert!(result.is_none());
+}
+
+#[test]
+fn test_stack_chk_fail() {
+    let proc = NativeStackChkFail;
+    assert!(proc.no_return());
+    assert_eq!(proc.num_args(), 0);
+    assert_eq!(proc.name(), "__stack_chk_fail");
+    let mut state = RustSimState::new("amd64").unwrap();
+    let result = proc.call(&mut state, &[]).unwrap();
+    assert!(result.is_none());
+}
