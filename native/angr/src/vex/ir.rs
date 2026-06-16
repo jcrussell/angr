deb6319b7672f8e1e028facef1719d5d62433c0f
Jon Crussell <joncrussell@gmail.com>
2026-06-16 02:23:58 +0000

    refactor: extract vex/ir.rs mod tests into ir_tests.rs sibling

--- native/angr/src/vex/ir.rs
+++ native/angr/src/vex/ir.rs
@@ -1746,29 +1746,5 @@ pub enum DirtyFx {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_ir_type_sizes() {
-        assert_eq!(IRType::I1.bits(), 1);
-        assert_eq!(IRType::I8.bits(), 8);
-        assert_eq!(IRType::I32.bits(), 32);
-        assert_eq!(IRType::I64.bits(), 64);
-        assert_eq!(IRType::V128.bits(), 128);
-    }
-
-    #[test]
-    fn test_ir_const_types() {
-        assert_eq!(IRConst::U8(42).get_type(), IRType::I8);
-        assert_eq!(IRConst::U32(42).get_type(), IRType::I32);
-        assert_eq!(IRConst::U64(42).get_type(), IRType::I64);
-    }
-
-    #[test]
-    fn test_irsb_creation() {
-        let irsb = IRSB::new(0x1000, VexArch::AMD64);
-        assert_eq!(irsb.addr, 0x1000);
-        assert_eq!(irsb.num_instructions(), 0);
-    }
-}
+#[path = "ir_tests.rs"]
+mod tests;
